6350f8b8db8b200034b41e459d681c4defcd7d64
Yasharth011 <yasharthgupta10@gmail.com>
2025-03-06 20:10:11 +0530

    gradient_descent: using argeparser with -w -b -alpha

--- LinearRegression/gradient_descent.py
+++ LinearRegression/gradient_descent.py
@@ -1,42 +1,66 @@
 import kagglehub
 import pandas as pd
+import argparse 
 
-path = kagglehub.dataset_download("andonians/random-linear-regression")
+# path = kagglehub.dataset_download("andonians/random-linear-regression")
 
-print("Path of Data Set : ", path)
+# adding  cmd line arguments 
+parser = argparse.ArgumentParser()
+parser.add_argument("-w", help="weight for gradient descen", type=float)
+parser.add_argument("-b", help="bias for gradient descen", type=float)
+parser.add_argument("-alpha", help="learning rate of gradient descen", type=float)
+args = parser.parse_args()
 
 
 def gradient_descent(alpha, data_set, w, b):
 
     m = data_set[data_set.columns[0]].count()
+
     for i in range(m):
+
         x = data_set.iloc[i, 0]
         y = data_set.iloc[i, 1]
+        
+        # if value is nan ignore 
         if (pd.isna(x) or pd.isna(y)):
             continue
-        f = w*x + b
+
+        f = w*x + b 
         w = w - alpha*(1/m)*(f - y)*x
         b = b - alpha*(1/m)*(f - y)
     return (w, b)
 
 
 def predict_value(x, w, b):
-    return w*x + b
+    return (w*x + b)
 
 
 def main():
-    data = pd.read_csv(path + "/train.csv")
+    data = pd.read_csv("train.csv")
 
-    w = float(input("Enter value for w : "))
-    b = float(input("Enter value for b : "))
-    # learning rate = 0.1 best for this data set
+    # parameters for gardient descent
+    w = args.w
+    b = args.b 
+    alpha = args.alpha
+
+    # performing gradient descent 
     w, b = gradient_descent(0.1, data, w, b)
-    test_data = pd.read_csv(path+"/test.csv")
-    rows = test_data[test_data.columns[0]].count()
-    for i in range(rows):
-        x = test_data.iloc[i, 0]
-        y = predict_value(x, w, b)
-        print("Predicted value : ", y)
+
+    # using testing data 
+    # test_data = pd.read_csv(path+"/test.csv")
+    # rows = test_data[test_data.columns[0]].count()
+    # print("test_x | test_y \t train_x | train_y")
+    # for i in range(rows):
+    #     x = test_data.iloc[i, 0]
+    #     y = predict_value(x, w, b)
+    #     print(x, ":", y, "|", data.iloc[i, 0], ":", data.iloc[i, 1])
+
+    # using user input 
+    input_value = float(input("Enter a number : "))
+    
+    y = predict_value(input_value, w, b)
+    # index = data.index(input_value)
+    print(input_value, ":", y)#, "|", data[input_value])
 
 
 if __name__ == "__main__":
